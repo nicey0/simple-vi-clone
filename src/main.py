f206a0609ffaaaf0d2b8920670ad0f5eb9a9b95b
nicey0 <nicey_pm@protonmail.com>
2020-06-09 10:43:37 -0300

    basic main=>mode interaction

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 import curses
 from modes.mode import Mode
 from modes.normal import Normal
+from messages import Message as M
+
+mode: Mode = None
+
+def draw_screen(scr: curses.window):
+    pass
 
 def main(scr: curses.window):
-    scr.clear()
-    scr.addstr("Hello!")
-    scr.refresh()
-    scr.getch()
+    while True:
+        scr.clear()
+        draw_screen(scr)
+        scr.refresh()
+        m = mode.process_key(scr.getch())
+        if m == M.BREAK:
+            break
 
 if __name__ == '__main__':
     curses.wrapper(main)
