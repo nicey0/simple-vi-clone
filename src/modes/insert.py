810bcd846a1f88086f768906dde8072a8b0471ab
nicey0 <nicey_pm@protonmail.com>
2020-06-11 13:46:57 -0300

    Switched to flake8 and made it happy

--- src/modes/insert.py
+++ src/modes/insert.py
@@ -3,13 +3,16 @@ import modes.mode as mode
 import modes.normal as normal
 import messages as m
 
+
 class Insert(mode.Mode):
     def __init__(self):
         self.highlights = False
+
     def process_key(self, key: int) -> tuple:
         if key == BS:
             return (m.Message.SWITCH, normal.Normal)
         else:
             return (m.Message.CONTINUE, 0)
+
     def __str__(self):
         return "insert"
