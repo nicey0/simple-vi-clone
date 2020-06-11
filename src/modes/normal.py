d19952c9907074e54783e42fc769f0ff92cc59a7
nicey0 <nicey@protonmail.ch>
2020-06-11 12:49:03 -0300

    Fixed import errors

--- src/modes/normal.py
+++ src/modes/normal.py
@@ -1,29 +1,30 @@
-from messages import Message as M
-from modes.mode import Mode
-from modes import insert
-from modes import visual
+import src.modes.mode as mode
+import src.modes.insert as insert
+import src.modes.normal as normal
+import src.modes.visual as visual
+import src.messages as m
 
-class Normal(Mode):
+class Normal(mode.Mode):
     def __init__(self):
         self.highlights = False
     def process_key(self, key: int) -> tuple:
         if key == ord('j'):
-            return (M.CURSOR, [1, 0])
+            return (m.Message.CURSOR, [1, 0])
         elif key == ord('k'):
-            return (M.CURSOR, [-1, 0])
+            return (m.Message.CURSOR, [-1, 0])
         elif key == ord('h'):
-            return (M.CURSOR, [0, -1])
+            return (m.Message.CURSOR, [0, -1])
         elif key == ord('l'):
-            return (M.CURSOR, [0, 1])
+            return (m.Message.CURSOR, [0, 1])
         elif key == ord('i'):
-            return (M.SWITCH, insert.Insert)
+            return (m.Message.SWITCH, insert.Insert)
         elif key == ord('v'):
-            return (M.SWITCH, visual.Visual)
+            return (m.Message.SWITCH, visual.Visual)
         elif key == ord('W'):
-            return (M.SAVE, 0)
+            return (m.Message.SAVE, 0)
         elif key == ord('q'):
-            return (M.BREAK, 0)
+            return (m.Message.BREAK, 0)
         else:
-            return (M.CONTINUE, 0)
+            return (m.Message.CONTINUE, 0)
     def __str__(self):
         return "normal"
