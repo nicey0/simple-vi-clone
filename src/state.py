7649254741d07b6e4fedf9150b55dc60eef2dc08
nicey0 <nicey_pm@protonmail.com>
2020-06-12 09:28:31 -0300

    No default lines in state

--- src/state.py
+++ src/state.py
@@ -6,7 +6,7 @@ class State:
     def __init__(self, scr: curses.window, filename: str = ""):
         self.scr: curses.window = scr
         self.filename: str = filename if filename != "" else None
-        self.content: list = ["testline0", "testline1", "testline2"]
+        self.content: list = [""]
         self.cursor: list = [0, 0]  # y, x
         self.highlighted: list = [-1, -1]  # start, current
         self.last_hl: bool = False  # if the program was highlighting lines in
